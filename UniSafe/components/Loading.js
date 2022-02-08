36a5ff830c88bbfb04c04fcb7dca8c7ea0edd27b
daniel <danielagurcia90@gmail.com>
2022-02-07 21:01:41 -0600

    Configuracion tailwindcss

---
+++ UniSafe/components/Loading.js
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import tw from "twrnc";
+
+export default function Loading() {
+  return (
+    <View style={tw`bg-slate-800 justify-center items-center h-full`}>
+      <Text style={tw`text-white text-xl font-bold`}>
+        Ustedes la verdad es que me pelan toda la verga 0tes malditos! .l.
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
---
+++ UniSafe/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./**/*.tsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: { transform: false, translate: false, boxShadow: false },
+};
