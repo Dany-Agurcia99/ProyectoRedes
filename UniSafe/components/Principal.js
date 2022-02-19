0da7ea7cc033ef9dff48f486c5f4389fe8b1c249
daniel <danielagurcia90@gmail.com>
2022-02-18 21:40:28 -0600

    Avance en pagina principal

--- UniSafe/components/Principal.js
+++ UniSafe/components/Principal.js
@@ -6,14 +6,14 @@ import Mapa from "../Imagenes/mapa.jpg"
 export default function Principal() {
     return (
         <View style={tw`flex bg-black h-full`}>
-            <View style={tw`flex-1 flex-row w-full h-full bg-yellow-100 justify-center items-center`}>
+            <View style={tw`flex-1 flex-row w-full h-full bg-orange-100 justify-center items-center`}>
                 {/*FLOATING BOX*/}
                 <View style={tw`self-end w-10/12 h-3/12 bg-slate-900 mb-5 rounded-3xl`}>
 
                 </View>
             </View>
             {/*NAVBAR*/}
-            <View style={tw`flex-initial flex-row h-18 w-5/6 mt-10 absolute self-center`}>
+            <View style={tw`flex-initial flex-row h-14 w-5/6 mt-14 absolute self-center`}>
                 <View style={tw`flex-none bg-slate-900 w-1/6 items-center justify-center rounded-bl-2xl rounded-tl-2xl`}>
                     <Text style={tw`text-xl font-bold text-white`}>
                         ...
